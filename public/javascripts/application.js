239f821c0e35eebbd66dcc02088c7c45189525c3
François Gaudreau <flash@imac-de-francois-gaudreau.local>
2009-08-28 13:36:37 -0400

    Hide core-controls until hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Event.observe(window, "load", function() {
+    $$("tr.core-controls").each(function(el) {
+        Event.observe(el.up("table"), "mouseover", function() {
+            el.show();
+        });
+        Event.observe(el.up("table"), "mouseout", function() {
+            el.hide();
+        });
+    });
+});
