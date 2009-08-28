0991a1fc1b3d9c286377ecc0cbc24350b314196c
François Gaudreau <flash@imac-de-francois-gaudreau.local>
2009-08-28 13:40:38 -0400

    Also hide the core-add-aspect controls

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,4 +9,13 @@ Event.observe(window, "load", function() {
             el.hide();
         });
     });
+    $$("input.add-aspect").each(function(el) {
+        el = el.up("td.core-add-aspect");
+        Event.observe(el.up("table"), "mouseover", function() {
+            el.show();
+        });
+        Event.observe(el.up("table"), "mouseout", function() {
+            el.hide();
+        });
+    });
 });
