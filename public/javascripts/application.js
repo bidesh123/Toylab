7d86eb6f45df22f809f3d370a0f6a2974cd41f05
François Gaudreau <flash@imac-de-francois-gaudreau.local>
2009-08-28 15:36:12 -0400

    Handle the case where there are no parent tables

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,20 +2,25 @@
 // This file is automatically included by javascript_include_tag :defaults
 Event.observe(window, "load", function() {
     $$("tr.core-controls").each(function(el) {
-        Event.observe(el.up("table"), "mouseover", function() {
-            el.show();
-        });
-        Event.observe(el.up("table"), "mouseout", function() {
-            el.hide();
-        });
+        var table = el.up("table");
+        if (table) {
+            Event.observe(el.up("table"), "mouseover", function() {
+                el.show();
+            });
+            Event.observe(el.up("table"), "mouseout", function() {
+                el.hide();
+            });
+        }
     });
     $$("input.add-aspect").each(function(el) {
         el = el.up("td.core-add-aspect");
-        Event.observe(el.up("table"), "mouseover", function() {
-            el.show();
-        });
-        Event.observe(el.up("table"), "mouseout", function() {
-            el.hide();
-        });
+        if (el) {
+            Event.observe(el.up("table"), "mouseover", function() {
+                el.show();
+            });
+            Event.observe(el.up("table"), "mouseout", function() {
+                el.hide();
+            });
+        }
     });
 });
