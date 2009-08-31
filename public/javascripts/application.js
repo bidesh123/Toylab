867a56f8b609ea4eca5a392f67eaa2d1068c0f27
François Beausoleil <francois@teksol.info>
2009-08-31 15:02:56 -0400

    Fade controls instead of appear

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,26 +7,11 @@ function installAutocomplete(el, url, parentName) {
 };
 
 Event.observe(window, "load", function() {
-    $$("tr.core-controls").each(function(el) {
+    $$("tr.core-controls", "td.core-add-aspect").each(function(el) {
         var table = el.up("table");
         if (table) {
-            Event.observe(el.up("table"), "mouseover", function() {
-                el.show();
-            });
-            Event.observe(el.up("table"), "mouseout", function() {
-                el.hide();
-            });
-        }
-    });
-    $$("input.add-aspect").each(function(el) {
-        el = el.up("td.core-add-aspect");
-        if (el) {
-            Event.observe(el.up("table"), "mouseover", function() {
-                el.show();
-            });
-            Event.observe(el.up("table"), "mouseout", function() {
-                el.hide();
-            });
+            Event.observe(table, "mouseover", function() { new Effect.Appear(el); });
+            Event.observe(table, "mouseout",  function() { new Effect.Fade(el); });
         }
     });
 
