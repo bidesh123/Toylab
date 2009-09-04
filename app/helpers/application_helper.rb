0efe09e7f1cd62258e34216cad80588c2b49630b
François Beausoleil <francois@teksol.info>
2009-09-04 12:16:42 -0400

    Edit whole page when editable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def editable_now?(card)
-    @editable_children.include?(card)
+    @editable_children && @editable_children.include?(card)
   end
 end
