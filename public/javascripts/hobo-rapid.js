427ae425b0907235d6f86b5c19b6f7c184761abf
François Gaudreau <flash@imac-de-francois-gaudreau.local>
2009-11-18 12:20:55 -0500

    temporary remove default body to set toy controls on remote

--- public/javascripts/hobo-rapid.js
+++ public/javascripts/hobo-rapid.js
@@ -763,7 +763,7 @@ Event.addBehavior({
          }
     },
 
-    // NOTE: Fixed the <br/>\n case (FB+FG)
+    // NOTE: Fixed the <br/>\n case (FB, then FG)
     // Else, JS would replace <br/>\n with \n\n, thereby doubling the number of lines
     '.text.in-place-edit, .markdown.in-place-edit, .textile.in-place-edit' : function (ev) {
         var ipe = Hobo._makeInPlaceEditor(this, {rows: 2})
